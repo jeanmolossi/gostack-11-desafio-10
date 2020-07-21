d0a578958d64cff6dcbbb4d7cfe0c8a7a3d1c18a
Jean Carlos <jean.carlosmolossi@gmail.com>
2020-07-21 13:34:24 -0300

    Finished another challenge

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -28,6 +28,9 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadFoods(): Promise<void> {
       // TODO LOAD FOODS
+      const response = await api.get(`foods`);
+
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -37,7 +40,12 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const addFood = await api.post(`foods`, {
+        ...food,
+        available: true,
+      });
+
+      setFoods([...foods, addFood.data]);
     } catch (err) {
       console.log(err);
     }
@@ -47,10 +55,24 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     // TODO UPDATE A FOOD PLATE ON THE API
+    await api
+      .put(`foods/${editingFood.id}`, {
+        ...editingFood,
+        ...food,
+      })
+      .then(() => {
+        const newFoodList = foods.map(f =>
+          f.id === editingFood.id ? { ...editingFood, ...food } : f,
+        );
+        setFoods(newFoodList);
+      });
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
     // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`foods/${id}`).then(response => console.log(response));
+    const foodsList = foods.filter(f => f.id !== id);
+    setFoods(foodsList);
   }
 
   function toggleModal(): void {
@@ -63,6 +85,8 @@ const Dashboard: React.FC = () => {
 
   function handleEditFood(food: IFoodPlate): void {
     // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    toggleEditModal();
   }
 
   return (
